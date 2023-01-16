ddf8b12bb930733b638386b411c7b5def3d2ee14
h0ngwon <w___on@naver.com>
2023-01-16 19:22:31 +0900

    refactor : refactoring

--- src/App.js
+++ src/App.js
@@ -1,43 +1,48 @@
+import { useState } from 'react';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
 
+const DUMMY_EXPENSES = [
+	{
+		id: 'e1',
+		title: '화장지',
+		amount: 10000,
+		date: new Date(2020, 7, 14),
+	},
+	{
+		id: 'e2',
+		title: 'TV',
+		amount: 790000,
+		date: new Date(2021, 2, 12),
+	},
+	{
+		id: 'e3',
+		title: '차 보험',
+		amount: 300000,
+		date: new Date(2021, 2, 28),
+	},
+	{
+		id: 'e4',
+		title: '책상',
+		amount: 500000,
+		date: new Date(2021, 5, 12),
+	},
+];
+
 const App = () => {
-	const expenses = [
-		{
-			id: 'e1',
-			title: '화장지',
-			amount: 10000,
-			date: new Date(2020, 7, 14),
-		},
-		{
-			id: 'e2',
-			title: 'TV',
-			amount: 790000,
-			date: new Date(2021, 2, 12),
-		},
-		{
-			id: 'e3',
-			title: '차 보험',
-			amount: 300000,
-			date: new Date(2021, 2, 28),
-		},
-		{
-			id: 'e4',
-			title: '책상',
-			amount: 500000,
-			date: new Date(2021, 5, 12),
-		},
-	];
+	const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+
 	const addExpenseHandler = (expense) => {
-		console.log('In app.js');
-		console.log(expenses)
-	}
+		setExpenses((prevExpenses) => {
+			return [expense, ...prevExpenses];
+		});
+	};
 	return (
 		<div>
-			<NewExpense onAddExpense={addExpenseHandler}/>
-			<Expenses items={expenses}/>
+			<NewExpense onAddExpense={addExpenseHandler} />
+			<Expenses items={expenses} />
 		</div>
 	);
-}
+};
 
 export default App;
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -10,6 +10,10 @@ const Expenses = (props) => {
 		setFilteredYear(year);
 	};
 
+	const filteredExpense = props.items.filter((expense) => {
+		return expense.date.getFullYear().toString() === filteredYear;
+	});
+
 	return (
 		<div>
 			<Card className='expenses'>
@@ -17,26 +21,18 @@ const Expenses = (props) => {
 					selected={filteredYear}
 					onAddExpenseFilter={addExpenseFilter}
 				/>
-				<ExpenseItem
-					title={props.items[0].title}
-					amount={props.items[0].amount}
-					date={props.items[0].date}
-				/>
-				<ExpenseItem
-					title={props.items[1].title}
-					amount={props.items[1].amount}
-					date={props.items[1].date}
-				/>
-				<ExpenseItem
-					title={props.items[2].title}
-					amount={props.items[2].amount}
-					date={props.items[2].date}
-				/>
-				<ExpenseItem
-					title={props.items[3].title}
-					amount={props.items[3].amount}
-					date={props.items[3].date}
-				/>
+				{filteredExpense.length === 0 && (
+					<p>항목이 존재하지 않습니다.</p>
+				)}
+				{filteredExpense.length > 0 &&
+					filteredExpense.map((expense) => (
+						<ExpenseItem
+							key={expense.id}
+							title={expense.title}
+							amount={expense.amount}
+							date={expense.date}
+						/>
+					))}
 			</Card>
 		</div>
 	);
